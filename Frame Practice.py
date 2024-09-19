e9d9a12fec3ab57f19eb1bf5d46fdeefabbe9711
KortSchaefer <schaeferkort@gmail.com>
2024-09-18 23:15:48 -0500

    test 5

--- Frame Practice.py
+++ Frame Practice.py
@@ -1,11 +1,11 @@
 from tkinter import *
 from PIL import ImageTk, Image
-
+'''Lol it didnt work'''
 root = Tk()
 root.title("Image Practice")
 root.iconbitmap(r"C:\Users\Kingc\Documents\Code\Python\Practice Images\code.ico")
 
-frame = LabelFrame(root, text="This is my frame....", padx=5, pady=5)
+frame = LabelFrame(root, text="This is my frame.... ", padx=5, pady=5)
 frame.pack(padx=10, pady=10)
 
 b = Button(frame,text="Dont Click Here!")
