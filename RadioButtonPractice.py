e1ce9645bed1934a8e4dc9313a1f840b400e3af6
KortSchaefer <schaeferkort@gmail.com>
2024-09-20 00:13:40 -0500

    Button Def Added

--- RadioButtonPractice.py
+++ RadioButtonPractice.py
@@ -4,13 +4,20 @@ from PIL import ImageTk, Image
 root= Tk()
 
 r = IntVar()
+r.set('1')
 r2 = IntVar()
 
-Radiobutton(root, text="Option 1", variable=r, value=1).pack()
-Radiobutton(root, text="Option 1", variable=r, value=2).pack()
 
-myLabel = Label(root, text=r.get())
-myLabel.pack()
+
+def clicked(value):
+    myLabel = Label(root, text=value)
+    myLabel.pack()
+
+Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: clicked(r.get())).pack()
+Radiobutton(root, text="Option 1", variable=r, value=2, command=lambda: clicked(r.get())).pack()
+
+b1 = Button(root, text="click Me", command=lambda: clicked(r.get()))
+b1.pack()
 
 Radiobutton(root, text="Option 1", variable=r2, value=1).pack()
 Radiobutton(root, text="Option 1", variable=r2, value=2).pack()
