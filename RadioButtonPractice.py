5e265676ec1f4b406140406a6ec20e56cbc2cc60
KortSchaefer <schaeferkort@gmail.com>
2024-09-20 00:07:07 -0500

    Uptate #1

--- RadioButtonPractice.py
+++ RadioButtonPractice.py
@@ -1,7 +1,18 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
-b3 = Button(text="Dont Click Here!")
-b3.pack()
-
 root= Tk()
+
+r = IntVar()
+r2 = IntVar()
+
+Radiobutton(root, text="Option 1", variable=r, value=1).pack()
+Radiobutton(root, text="Option 1", variable=r, value=2).pack()
+
+myLabel = Label(root, text=r.get())
+myLabel.pack()
+
+Radiobutton(root, text="Option 1", variable=r2, value=1).pack()
+Radiobutton(root, text="Option 1", variable=r2, value=2).pack()
+
+root.mainloop()
