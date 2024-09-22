35ac605769d9ecd1cc90b536e12473ba05f3a51a
KortSchaefer <schaeferkort@gmail.com>
2024-09-22 15:26:48 -0500

    Added functionality to yes or no

--- Message box Practice.py
+++ Message box Practice.py
@@ -7,9 +7,15 @@ root = Tk()
 root.title("Image Practice")
 root.iconbitmap(r"C:\Users\Kingc\Documents\Code\Python\Practice Images\code.ico")
 
-def popup():
-    messagebox.showinfo("This is my popup", "hello World")
+'''You can change the showinfo to many things depending on use case. ex. (askyesno, askokcancel, etc.)'''
 
+def popup():
+    responce=messagebox.askyesno("This is my popup", "hello World")
+    Label(root, text=responce).pack()
+    if responce == 1:
+        Label(root, text="you clicked yes").pack()
+    else:
+        Label(root, text="you clicked no").pack()
 
 Button(root, text="Popup", command=popup).pack()
 
