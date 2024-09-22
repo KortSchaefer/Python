2faf72b7e6fab7e062b8abf676cf00fc4c54146f
KortSchaefer <schaeferkort@gmail.com>
2024-09-22 15:18:23 -0500

    Created format style

---
+++ Message box Practice.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Image Practice")
+root.iconbitmap(r"C:\Users\Kingc\Documents\Code\Python\Practice Images\code.ico")
+
+'''Code Goes Here'''
+
+root.mainloop()
