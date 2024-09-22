e99a4f40a6fe0b367b590cad9a69518adb01faed
KortSchaefer <schaeferkort@gmail.com>
2024-09-22 15:22:29 -0500

    Basic popup window

--- Message box Practice.py
+++ Message box Practice.py
@@ -1,10 +1,16 @@
 from tkinter import *
 from PIL import ImageTk, Image
+'''You need a module for this one'''
+from tkinter import messagebox
 
 root = Tk()
 root.title("Image Practice")
 root.iconbitmap(r"C:\Users\Kingc\Documents\Code\Python\Practice Images\code.ico")
 
-'''Code Goes Here'''
+def popup():
+    messagebox.showinfo("This is my popup", "hello World")
+
+
+Button(root, text="Popup", command=popup).pack()
 
 root.mainloop()
