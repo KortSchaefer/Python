cd999e1821cbf66fe3eefc90868c9e5a888cc7c5
KortSchaefer <schaeferkort@gmail.com>
2024-09-23 23:27:13 -0500

    Actual Layout

--- POS Version -1.py
+++ POS Version -1.py
@@ -9,40 +9,40 @@ root.iconbitmap(r"C:\Users\Kingc\Documents\Code\Python\Practice Images\code.ico"
 ### |  |  |  |
 ### |xx|  |  |
 ### |  |  |  |
-orderFrame = LabelFrame(root, text="Order Box", padx=50, pady=50)
-orderFrame.grid(column=0, row=1, padx=10, pady=10)
+orderFrame = LabelFrame(root, text="Order Box", padx=100, pady=300)
+orderFrame.grid(column=0, row=1, padx=(15, 2), pady=2)
 orderBox= Label(orderFrame, text='Box').pack()
 
 
 ### |  |  |  |
 ### |  |  |xx|
 ### |  |  |  |
-entryFrame = LabelFrame(root, text="Order Box", padx=50, pady=50)
-entryFrame.grid(column=2, row=1, padx=10, pady=10)
+entryFrame = LabelFrame(root, text="Entry Box", padx=250, pady=300)
+entryFrame.grid(column=2, row=1, padx=(2,15), pady=2)
 orderBox= Label(entryFrame, text='Box').pack()
 
 
 ### |  |  |  |
 ### |  |xx|  |
 ### |  |  |  |
-scrollFrame = LabelFrame(root, text="Order Box", padx=50, pady=50)
-scrollFrame.grid(column=1, row=1, padx=10, pady=10)
+scrollFrame = LabelFrame(root, text="Scroll Box", padx=30, pady=300)
+scrollFrame.grid(column=1, row=1, padx=2, pady=2)
 orderBox= Label(scrollFrame, text='Box').pack()
 
 
 ### |  |  |  |
 ### |  |  |  |
 ### |xx|xx|xx|
-bottomFrame = LabelFrame(root, text="Order Box", padx=50, pady=50)
-bottomFrame.grid(column=1, row=3, padx=10, pady=10)
+bottomFrame = LabelFrame(root, text="Bottom Box", padx=430, pady=50)
+bottomFrame.grid(column=0, row=3, padx=2, pady=2, columnspan=3)
 orderBox= Label(bottomFrame, text='Box').pack()
 
 
 ### |xx|xx|xx|
 ### |  |  |  |
 ### |  |  |  |
-topFrame = LabelFrame(root, text="Order Box", padx=50, pady=50)
-topFrame.grid(column=1, row=0, padx=10, pady=10)
+topFrame = LabelFrame(root, text="Top Box", padx=430, pady=50)
+topFrame.grid(column=0, row=0, padx=2, pady=2,columnspan=4)
 orderBox= Label(topFrame, text='Box').pack()
 
 root.mainloop()
