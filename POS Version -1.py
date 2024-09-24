7ff43241faa32a57ef7821e5a88e3f1f98333448
KortSchaefer <schaeferkort@gmail.com>
2024-09-23 23:56:15 -0500

    Changed padx/y to height, currently not working

--- POS Version -1.py
+++ POS Version -1.py
@@ -4,20 +4,24 @@ from PIL import ImageTk, Image
 root = Tk()
 root.title("POS System")
 root.iconbitmap(r"C:\Users\Kingc\Documents\Code\Python\Practice Images\code.ico")
+root.configure(bg="lightblue")
+
+
 
 
 ### |  |  |  |
 ### |xx|  |  |
 ### |  |  |  |
-orderFrame = LabelFrame(root, text="Order Box", padx=100, pady=300)
-orderFrame.grid(column=0, row=1, padx=(15, 2), pady=2)
+orderFrame = LabelFrame(root, text="Order Box", width=100, height=300, relief='raised', bg='white')
+orderFrame.grid_propagate(False)
+orderFrame.grid(column=0, row=1, padx=(15, 2), pady=2, sticky=NSEW)
 orderBox= Label(orderFrame, text='Box').pack()
 
 
 ### |  |  |  |
 ### |  |  |xx|
 ### |  |  |  |
-entryFrame = LabelFrame(root, text="Entry Box", padx=250, pady=300)
+entryFrame = LabelFrame(root, text="Entry Box", width=250, height=300, relief='raised',)
 entryFrame.grid(column=2, row=1, padx=(2,15), pady=2)
 orderBox= Label(entryFrame, text='Box').pack()
 
@@ -25,15 +29,19 @@ orderBox= Label(entryFrame, text='Box').pack()
 ### |  |  |  |
 ### |  |xx|  |
 ### |  |  |  |
-scrollFrame = LabelFrame(root, text="Scroll Box", padx=30, pady=300)
-scrollFrame.grid(column=1, row=1, padx=2, pady=2)
+scrollFrame = LabelFrame(root, text="Scroll Box", width=40, height=300, relief='raised',)
+scrollFrame.grid_propagate(False)
+scrollFrame.grid(column=1, row=1, padx=2, pady=2,)
 orderBox= Label(scrollFrame, text='Box').pack()
+
+### Scroll box buttons
+upArrow=Button(scrollFrame, text="up").pack()
 
 
 ### |  |  |  |
 ### |  |  |  |
 ### |xx|xx|xx|
-bottomFrame = LabelFrame(root, text="Bottom Box", padx=430, pady=50)
+bottomFrame = LabelFrame(root, text="Bottom Box", width=430, height=20, relief='raised',)
 bottomFrame.grid(column=0, row=3, padx=2, pady=2, columnspan=3)
 orderBox= Label(bottomFrame, text='Box').pack()
 
@@ -41,7 +49,7 @@ orderBox= Label(bottomFrame, text='Box').pack()
 ### |xx|xx|xx|
 ### |  |  |  |
 ### |  |  |  |
-topFrame = LabelFrame(root, text="Top Box", padx=430, pady=50)
+topFrame = LabelFrame(root, text="Top Box", width=430, height=20, relief='raised',)
 topFrame.grid(column=0, row=0, padx=2, pady=2,columnspan=4)
 orderBox= Label(topFrame, text='Box').pack()
 
