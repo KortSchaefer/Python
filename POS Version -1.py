613b60e2ea9d0df44f3e668404abc3fde00bbc93
KortSchaefer <schaeferkort@gmail.com>
2024-09-23 23:18:17 -0500

    fixed comments

--- POS Version -1.py
+++ POS Version -1.py
@@ -5,22 +5,42 @@ root = Tk()
 root.title("POS System")
 root.iconbitmap(r"C:\Users\Kingc\Documents\Code\Python\Practice Images\code.ico")
 
+
+### |  |  |  |
+### |xx|  |  |
+### |  |  |  |
 orderFrame = LabelFrame(root, text="Order Box", padx=50, pady=50)
 orderFrame.grid(column=0, row=1, padx=10, pady=10)
 orderBox= Label(orderFrame, text='Box').pack()
 
+
+### |  |  |  |
+### |  |  |xx|
+### |  |  |  |
 entryFrame = LabelFrame(root, text="Order Box", padx=50, pady=50)
 entryFrame.grid(column=2, row=1, padx=10, pady=10)
 orderBox= Label(entryFrame, text='Box').pack()
 
+
+### |  |  |  |
+### |  |xx|  |
+### |  |  |  |
 scrollFrame = LabelFrame(root, text="Order Box", padx=50, pady=50)
 scrollFrame.grid(column=1, row=1, padx=10, pady=10)
 orderBox= Label(scrollFrame, text='Box').pack()
 
+
+### |  |  |  |
+### |  |  |  |
+### |xx|xx|xx|
 bottomFrame = LabelFrame(root, text="Order Box", padx=50, pady=50)
 bottomFrame.grid(column=1, row=3, padx=10, pady=10)
 orderBox= Label(bottomFrame, text='Box').pack()
 
+
+### |xx|xx|xx|
+### |  |  |  |
+### |  |  |  |
 topFrame = LabelFrame(root, text="Order Box", padx=50, pady=50)
 topFrame.grid(column=1, row=0, padx=10, pady=10)
 orderBox= Label(topFrame, text='Box').pack()
